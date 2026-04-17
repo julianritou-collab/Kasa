cdb8a27a427f0f7113973894ec2df62cdc2ae8e0
Julian Ritou <julian.ritou@gmail.com>
2026-04-17 18:07:51 +0200

    Etape3: Mise en page du footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,12 @@
+import logo from "../assets/logo.png";
+
 function Footer() {
   return (
     <footer>
-      <p>© 2026 Kasa. Tous droits réservés</p>
+      <div className="footer__inner">
+        <img src={logo} alt="logo deKasa" className="footer__logo" />
+        <p className="footer__text">© 2026 Kasa. All<span className="footer__break"> </span>rights reserved</p>
+      </div>
     </footer>
   );
 }
