3521b47059eacab5bfc566d303c7ceeb762efd8f
Julian Ritou <julian.ritou@gmail.com>
2026-04-18 12:51:31 +0200

    correction accessibilité Header

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer>
       <div className="footer__inner">
-        <img src={logo} alt="logo deKasa" className="footer__logo" />
+        <img src={logo} alt="Kasa" className="footer__logo" />
         <p className="footer__text">© 2026 Kasa. All<span className="footer__break"> </span>rights reserved</p>
       </div>
     </footer>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,8 @@ function Header() {
   return (
     <header>
       <div className="container header__inner">
-        <img src={logo} alt="logo deKasa" className="header__logo" />
-        <nav className="header__nav">
+        <img src={logo} alt="Kasa" className="header__logo" />
+        <nav className="header__nav" aria-label="Navigation principale">
           <NavLink to="/" end className={({ isActive }) => isActive ? "active" : ""}>Accueil</NavLink>
           <NavLink to="/about" className={({ isActive }) => isActive ? "active" : ""}>À propos</NavLink>
         </nav>
