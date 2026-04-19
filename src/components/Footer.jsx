9be052722c08813cc6e05fb05eb2aad97feb16f7
Julian Ritou <julian.ritou@gmail.com>
2026-04-19 14:58:17 +0200

    Etape6: page "not found" avec design Kasa

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     <footer>
       <div className="footer__inner">
         <img src={logo} alt="Kasa" className="footer__logo" />
-        <p className="footer__text">© 2026 Kasa. All<span className="footer__break"> </span>rights reserved</p>
+        <p className="footer__text">© 2026 Kasa. All<span className="newline-mobile"> </span>rights reserved</p>
       </div>
     </footer>
   );
